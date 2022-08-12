5b4480b3bb19aaba17f3737fb3068d5e7ca180f6
geekbrains <info@geekbrains.ru>
2022-08-12 19:51:25 +0300

    Add code

---
+++ src/Main.java
@@ -0,0 +1,63 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Integer[] arr = {5, 1, 7, 10, 22};
+        System.out.println("Задача №1 Меняет два элемента массива местами.");
+        System.out.println("Было: " + Arrays.toString(arr));
+        swapElements(arr, 0, 4); // Указываем индексы массива которые хотим поменять местами
+        System.out.println("Стало: " + Arrays.toString(arr));
+        System.out.println();
+
+        System.out.println("Задача №2 Пересыпаем фрукты из текущей коробки в другую.");
+        Orange orange = new Orange();
+        Apple apple = new Apple();
+        Box<Orange> orangeBox1 = new Box();
+        Box<Orange> orangeBox2 = new Box();
+        Box<Apple> appleBox = new Box();
+
+        for (int i = 0; i < 3; i++) {
+            orangeBox1.add(new Orange());
+        }
+
+        for (int i = 0; i < 4; i++) {
+            orangeBox2.add(new Orange());
+        }
+        for (int i = 0; i < 6; i++) {
+            appleBox.add(new Apple());
+        }
+
+
+        orangeBox1.info();
+        orangeBox2.info();
+        appleBox.info();
+
+        Float orange1Weigth = orangeBox1.getWeight();
+        Float orange2Weigth = orangeBox2.getWeight();
+        Float appleWeigth = appleBox.getWeight();
+        System.out.println("Вес коробки 1 с апельсинами: " + orange1Weigth);
+        System.out.println("Вес коробки 2 с апельсинами: " + orange2Weigth);
+        System.out.println("Вес коробки с яблоками: " + appleWeigth);
+
+        System.out.println("Сравнить вес orangeBox1 и appleBox: " + orangeBox1.compare(appleBox));
+        System.out.println("Сравнить вес orangeBox2 и appleBox: " + orangeBox2.compare(appleBox));
+
+        orangeBox1.moveAt(orangeBox2);
+
+        orangeBox1.info();
+        orangeBox2.info();
+        appleBox.info();
+    }
+
+    private static <T> void swapElements(T[] array, int index1, int index2) {
+        T temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+
+    private static <E> List<E> convertToList(E[] array) {
+        return Arrays.asList(array);
+    }
+}
